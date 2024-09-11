39cbcb571464e73271297ade799483a4acebba7b
Leah Perri <perrileah2023@gmail.com>
2024-09-11 18:58:27 -0500

    deploy attempt with adjusted lambda

--- lambdas/slack-event-function/handler.py
+++ lambdas/slack-event-function/handler.py
@@ -1,12 +1,31 @@
 import json
-
-print('Loading function')
-
+import os
 
 def lambda_handler(event, context):
-    #print("Received event: " + json.dumps(event, indent=2))
-    print("value1 = " + event['key1'])
-    print("value2 = " + event['key2'])
-    print("value3 = " + event['key3'])
-    return event['key1']  # Echo back the first key value
-    #raise Exception('Something went wrong')
+    # Simulated incoming Slack event
+    slack_event = {
+        "event": {
+            "type": "message",
+            "text": "What is our PTO policy?",
+            "user": "U12345678"
+        }
+    }
+
+    # Extract message and user_id from the simulated event
+    message = slack_event['event']['text']
+    user_id = slack_event['event']['user']
+
+    # Prepare a response (or perform another action)
+    response_message = {
+        'message': message,
+        'user_id': user_id
+    }
+
+    # Print the response message (for debugging purposes)
+    print(json.dumps(response_message))
+
+    # Return a response indicating that the processing is complete
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Message processed successfully')
+    }
